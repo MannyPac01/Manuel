2ef56d7569464c4a7170826383ac65ef90ce3a96
manuel <mannymann7@gmail.com>
2020-09-08 00:01:28 +0200

    Working with the for statement, and some code challenges.

--- projects/java/Main.java
+++ projects/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(12, 10000));
+//        System.out.println(NumberOfDaysInMonth.getDaysInMonth(12, 10000));
+        System.out.println(ForStatement.sumOdd.sumOdd(100, 1000));
     }
 }
